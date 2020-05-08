74a1edff81b9a15b55d76fc48584dc25afb24fa8
Cadet <cadet@cadet-vm.ninja>
2020-05-07 22:07:15 -0400

    Add existign file

--- AMIApp/forms.py
+++ AMIApp/forms.py
@@ -4,8 +4,6 @@ from django.db import models
 from .models import Position, Cadet_Has_Superior, Rooms, Cadets, Inspections, Gigs
 
 class InspectionsForm(forms.ModelForm):
-    #Pass = forms.BooleanField()
-    #Comments = forms.CharField(max_length=5000)
     class Meta:
         model = Inspections
         fields = ('room','Date','gigs','Pass','Comments')
